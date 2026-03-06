529e4b87beb5245b1e6441ac515106515610c895
greyhaven-jpn <rafidfikri@gmail.com>
2026-03-06 20:53:59 +0900

    Initial Commit

---
+++ ImageCompressor/ImageCompressorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCompressorApp.swift
+//  ImageCompressor
+//
+//  Created by Rafid on 2026/03/06.
+//
+
+import SwiftUI
+
+@main
+struct ImageCompressorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageCompressorTests/ImageCompressorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCompressorTests.swift
+//  ImageCompressorTests
+//
+//  Created by Rafid on 2026/03/06.
+//
+
+import Testing
+@testable import ImageCompressor
+
+struct ImageCompressorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
